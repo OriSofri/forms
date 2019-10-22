7148435ffb5483924045e9ee91d723ea0901d416
ori sofri <orisofri@gmail.com>
2019-10-22 21:36:04 +0300

    created register.html modifications still needed

--- forms.py
+++ forms.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, url_for 
-from formStructs import *
+from flask import Flask, render_template, url_for, redirect
+from formStructs import RegistrationForm,LoginForm 
 app = Flask(__name__)
+
+app.config['SECRET_KEY']='7bcdf801ed94d527b1771a596b5c93b8'
+
 
 @app.route('/')
 @app.route('/Home')
 def home():
     return render_template('home.html')
 
-@app.route('/Registration')
+@app.route('/register', methods=['GET', 'POST'])
 def registration():
-    return ''
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form= form)
 
-    
-@app.route('/Login')
+
+@app.route('/login')
 def login():
-    return ''
+    form= LoginForm()
+    return render_template('login,html', title='Login', form= form)
 
 #allowing python to run the file  
 if __name__ == '__main__':
